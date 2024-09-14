ae2f84f01880749e7ed2fab630740866f3debb7e
zqhgithubuser <129922525+zqhgithubuser@users.noreply.github.com>
2024-09-14 06:51:09 +0000

    解包优于索引

--- 05-unpack.py
+++ 05-unpack.py
@@ -2,11 +2,11 @@
 snacks = [("bacon", 350), ("donut", 240), ("muffin", 190)]
 
 for i in range(len(snacks)):
-  item = snacks[i]
-  name = item[0]
-  calories = item[1]
-  print(f"#{i + 1}: {name} has {calories} calories")
+    item = snacks[i]
+    name = item[0]
+    calories = item[1]
+    print(f"#{i + 1}: {name} has {calories} calories")
 
 # 解包
 for rank, (name, calories) in enumerate(snacks, 1):
-  print(f"#{rank}: {name} has {calories} calories")
+    print(f"#{rank}: {name} has {calories} calories")
