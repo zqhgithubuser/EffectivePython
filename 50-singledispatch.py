56e5b41b9b39c4b1cc0aae8f3abf794080e001c5
zqhgithubuser <129922525+zqhgithubuser@users.noreply.github.com>
2024-09-21 01:40:25 +0000

    考虑functools.singledispatch函数式编程而不是类多态

---
+++ 50-singledispatch.py
@@ -0,0 +1,82 @@
+import functools
+
+
+@functools.singledispatch
+def my_evaluate(node):
+    raise NotImplementedError
+
+
+@functools.singledispatch
+def my_pretty(node):
+    raise NotImplementedError
+
+
+########################################
+class Integer:
+
+    def __init__(self, value):
+        self.value = value
+
+
+@my_evaluate.register(Integer)
+def _(node):
+    return node.value
+
+
+@my_pretty.register(Integer)
+def _(node):
+    return repr(node.value)
+
+
+########################################
+class Add:
+
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+@my_evaluate.register(Add)
+def _(node):
+    left = my_evaluate(node.left)
+    right = my_evaluate(node.right)
+    return left + right
+
+
+@my_pretty.register(Add)
+def _(node):
+    left_str = my_pretty(node.left)
+    right_str = my_pretty(node.right)
+    return f'({left_str} + {right_str})'
+
+
+########################################
+class Multiply:
+
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+@my_evaluate.register(Multiply)
+def _(node):
+    left = my_evaluate(node.left)
+    right = my_evaluate(node.right)
+    return left * right
+
+
+@my_pretty.register(Multiply)
+def _(node):
+    left_str = my_pretty(node.left)
+    right_str = my_pretty(node.right)
+    return f'({left_str} * {right_str})'
+
+
+tree = Multiply(
+    Add(Integer(3), Integer(5)),
+    Add(Integer(4), Integer(7)),
+)
+
+print(my_pretty(tree))
+result = my_evaluate(tree)
+print(result)
