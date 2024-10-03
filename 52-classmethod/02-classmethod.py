a0d891289bdfa6d4bc5f0fa8666a08b981ac8a3e
zqhgithubuser <129922525+zqhgithubuser@users.noreply.github.com>
2024-10-03 17:20:50 +0000

    使用@classmethod自定义构造函数，是联系多个类的通用方式

---
+++ 52-classmethod/02-classmethod.py
@@ -0,0 +1,95 @@
+import os
+import random
+from threading import Thread
+
+
+class GenericInputData:
+
+    def read(self):
+        raise NotImplementedError
+
+    @classmethod
+    def generate_inputs(cls, config):
+        raise NotImplementedError
+
+
+class PathInputData(GenericInputData):
+
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
+
+    @classmethod
+    def generate_inputs(cls, config):
+        data_dir = config["data_dir"]
+        for name in os.listdir(data_dir):
+            yield cls(os.path.join(data_dir, name))
+
+
+class GenericWorker:
+
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+
+    def map(self):
+        raise NotImplementedError
+
+    def reduce(self, other):
+        raise NotImplementedError
+
+    @classmethod
+    def create_workers(cls, input_class, config):
+        workers = []
+        for input_data in input_class.generate_inputs(config):
+            workers.append(cls(input_data))
+        return workers
+
+
+class LineCountWorker(GenericWorker):
+
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count('\n')
+
+    def reduce(self, other):
+        self.result += other.result
+
+
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    first, *rest = workers
+    for worker in rest:
+        first.reduce(worker)
+    return first.result
+
+
+def mapreduce(worker_class, input_class, config):
+    workers = worker_class.create_workers(input_class, config)
+    return execute(workers)
+
+
+# 测试函数
+def write_test_files(tmpdir):
+    os.makedirs(tmpdir)
+    for i in range(100):
+        with open(os.path.join(tmpdir, str(i)), "w") as f:
+            f.write("\n" * random.randint(0, 100))
+
+
+if __name__ == '__main__':
+    tmpdir = "test_inputs"
+    write_test_files(tmpdir)
+
+    config = {"data_dir": tmpdir}
+    result = mapreduce(LineCountWorker, PathInputData, config)
+    print(f"There are {result} lines")
